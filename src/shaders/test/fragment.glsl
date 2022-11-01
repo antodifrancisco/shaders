3b8d1a4e0bde47d547f9d0ad6e78e8f53d7852d0
Antonio Di Francesco <antonio@jamroom.xyz>
2022-11-01 09:52:46 -0600

    learning shaders

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,6 +1,8 @@
 precision mediump float;
 
-    void main()
-    {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
+varying float vRandom;
+
+void main()
+{
+        gl_FragColor = vec4(vRandom, vRandom * 0.5, 1.0, 1.0);
+}
